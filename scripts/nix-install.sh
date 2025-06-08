aa863436dbd362fd0b178e4716b77f41992aab50
Tyler <3821892+kolatra@users.noreply.github.com>
2025-06-08 12:20:39 -0600

    add steps to nix install script

--- scripts/nix-install.sh
+++ scripts/nix-install.sh
@@ -1,29 +1,31 @@
 #!/run/current-system/sw/bin/bash
 
-# make partitions
+echo '[*] Making partitions...'
 parted /dev/vda -- mklabel gpt
 parted /dev/vda -- mkpart root ext4 512MB -8GB
 parted /dev/vda -- mkpart swap linux-swap -8GB 100%
 parted /dev/vda -- mkpart ESP fat32 1MB 512MB
 parted /dev/vda -- set 3 esp on
 
-# format partitions
+echo '[*] Formatting partitions...'
 mkfs.ext4 -L nixos /dev/vda1
 mkswap -L swap /dev/vda2
 mkfs.fat -F 32 -n boot /dev/vda3
 
-# install
+echo '[*] Mounting partitions...'
 mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot
 mount -o umask=077 /dev/disk/by-label/boot /mnt/boot
 
-mkdir -p /mnt/bin
-ln -s /run/current-system/sw/bin/bash /mnt/bin/bash
+echo '[*] Updating hardware config...'
 
 cp -r ~/dotfiles/laptop /mnt/etc/nixos
 rm /mnt/etc/nixos/hardware-configuration.nix
 nixos-generate-config --root /mnt
 
+echo '[*] Installing NixOS...'
 nixos-install
+
+read -p "Press any key to restart the system"
 reboot
 
