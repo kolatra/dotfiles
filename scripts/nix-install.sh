6cc31813d20056f207bd023da419b774f31b3326
Tyler <3821892+kolatra@users.noreply.github.com>
2025-06-08 21:22:39 -0600

    deploy script to laptop

--- scripts/nix-install.sh
+++ scripts/nix-install.sh
@@ -1,8 +1,10 @@
 #!/run/current-system/sw/bin/bash
 
-DISK=/dev/vda
+DISK=/dev/nvme0n1
 
 PASSWORD='horse'
+
+read -p '[!!!] Press any key to continue, this will erase your drive!'
 
 echo '[*] Making partitions...'
 parted $DISK -- mklabel gpt
@@ -12,18 +14,18 @@ parted $DISK -- mkpart ESP fat32 1MB 512MB
 parted $DISK -- set 3 esp on
 
 echo '[*] Formatting partitions...'
-mkfs.fat -F 32 -n boot "${DISK}3"
-echo -n "${PASSWORD}" | cryptsetup luksFormat --label="NixOS-Encrypted" "${DISK}1" -
-cryptsetup config "${DISK}1" --label "NixOS-Encrypted"
-echo -n "${PASSWORD}" | cryptsetup open "${DISK}1" cryptroot -
+mkfs.fat -F 32 -n boot "${DISK}p3"
+echo -n "${PASSWORD}" | cryptsetup luksFormat --label="NixOS-Encrypted" "${DISK}p1" -
+cryptsetup config "${DISK}p1" --label "NixOS-Encrypted"
+echo -n "${PASSWORD}" | cryptsetup open "${DISK}p1" cryptroot -
 mkfs.ext4 -L "NixOS-Root" /dev/mapper/cryptroot
-mkswap -L "NixOS-Swap" "${DISK}2"
+mkswap -L "NixOS-Swap" "${DISK}p2"
 
 echo '[*] Mounting partitions...'
 mount /dev/mapper/cryptroot /mnt
 mkdir -p /mnt/boot
-mount -o umask=077 "${DISK}3" /mnt/boot
-swapon "${DISK}2"
+mount -o umask=077 "${DISK}p3" /mnt/boot
+swapon "${DISK}p2"
 
 echo '[*] Updating hardware config...'
 mkdir -p /mnt/etc/nixos
