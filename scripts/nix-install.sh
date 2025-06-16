591d6ddc6fa897493482c75f78150c6fd7cf417c
Tyler <3821892+kolatra@users.noreply.github.com>
2025-06-15 23:02:59 -0600

    make sure to use luks2

--- scripts/nix-install.sh
+++ scripts/nix-install.sh
@@ -4,7 +4,7 @@ DISK=/dev/nvme0n1
 
 PASSWORD='horse'
 
-read -p '[!!!] Press any key to continue, this will erase your drive!'
+read -p '[!!!] Press enter to continue, this will erase your drive!'
 
 echo '[*] Making partitions...'
 parted $DISK -- mklabel gpt
@@ -15,7 +15,7 @@ parted $DISK -- set 3 esp on
 
 echo '[*] Formatting partitions...'
 mkfs.fat -F 32 -n boot "${DISK}p3"
-echo -n "${PASSWORD}" | cryptsetup luksFormat --label="NixOS-Encrypted" "${DISK}p1" -
+echo -n "${PASSWORD}" | cryptsetup luksFormat --type luks2 --label="NixOS-Encrypted" "${DISK}p1" -
 cryptsetup config "${DISK}p1" --label "NixOS-Encrypted"
 echo -n "${PASSWORD}" | cryptsetup open "${DISK}p1" cryptroot -
 mkfs.ext4 -L "NixOS-Root" /dev/mapper/cryptroot
@@ -27,11 +27,8 @@ mkdir -p /mnt/boot
 mount -o umask=077 "${DISK}p3" /mnt/boot
 swapon "${DISK}p2"
 
-cp ../nix/hosts/laptop/configuration.nix /mnt/etc/nixos
-nixos-generate-config --root /mnt
-
 echo '[*] Installing NixOS...'
-nixos-install --no-root-passwd
+nixos-install --no-root-passwd --flake ~/dotfiles/nix#tethys
 
 if [ $? -eq 0 ]; then
   reboot
