d02602017ffc5140c5e9ec87e91b97ee881bd12c
Tyler <3821892+kolatra@users.noreply.github.com>
2025-06-29 17:27:54 -0600

    sync

--- nix/hosts/vm/configuration.nix
+++ nix/hosts/vm/configuration.nix
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-=======
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
->>>>>>> 7f29cec (sync)
 { config, lib, pkgs, ... }:
 
 {
@@ -13,32 +6,17 @@
       ./hardware-configuration.nix
     ];
 
-<<<<<<< HEAD
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.plymouth.enable = true;
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  networking.hostName = "vm";
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "America/Edmonton";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  users.users.root.password = "root";
-
-=======
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.plymouth.enable = true;
 
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "vm"; # Define your hostname.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  users.users.root.password = "root";
 
   services.greetd = {
     enable = true;
@@ -46,28 +24,15 @@
     settings = {
       default_session = {
         user = "root";
-	command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd /run/current-system/sw/bin/bash";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd /run/current-system/sw/bin/bash";
       };
     };
   };
 
   time.timeZone = "America/Edmonton";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     tree
-  #   ];
-  # };
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
->>>>>>> 7f29cec (sync)
   environment.systemPackages = with pkgs; [
     neovim
     wget
